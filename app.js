fbd8e9f3ac9719eb9801d691da7b62881154a712
BBD\james.barrett <james.barrett@brightblueday.com>
2016-12-22 17:17:23 +0000

    JSON Received From API

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const button = document.querySelector("button");
+const input = document.querySelector("input");
 
+button.addEventListener("click", function() {
+
+  const url = "https://en.wikipedia.org/w/api.php?callback=?";
+  const data = {
+    "action" : "query",
+    "titles" : input.value,
+    "prop" : "revisions",
+    "rvprop" : "content",
+    "format" : "json",
+  }
+
+  function callback(data) {
+    console.log(data);
+  }
+
+  $.getJSON(url, data, callback);
+});
