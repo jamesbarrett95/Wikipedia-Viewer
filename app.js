67ced80b8e8ab8d164278c503ea2da5bdbb948c9
BBD\james.barrett <james.barrett@brightblueday.com>
2017-01-04 11:28:28 +0000

    JSON data displayed on the page

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 const button = document.querySelector("button");
 const input = document.querySelector("input");
+const container = document.getElementById("container");
+const resultsarea = document.getElementById("resultsarea");
 
 button.addEventListener("click", function() {
 
-  const url = "https://en.wikipedia.org/w/api.php?callback=?";
-  const data = {
-    "action" : "query",
-    "titles" : input.value,
-    "prop" : "revisions",
-    "rvprop" : "content",
-    "format" : "json",
+  // Remove previous results
+  while (resultsarea.firstChild) {
+    resultsarea.removeChild(resultsarea.firstChild);
   }
 
+  // AJAX URL and query strings values
+  const url = "https://en.wikipedia.org/w/api.php?callback=?";
+  const data = {
+    "action" : "opensearch",
+    "search" : input.value,
+    "format" : "json"
+  }
+
+  // AJAX Callback function
   function callback(data) {
     console.log(data);
+    let h2 = document.createElement('h2');
+    let searchResults = data[1];
+    let searchDescriptions = data[2];
+    let hrefs = data[3];
+
+    h2.textContent = "Wikipedia Entries For " + input.value;
+    resultsarea.appendChild(h2);
+
+    // Iterate through results and display them on the screen
+    for(let i = 0; i < searchResults.length; i++) {
+      let entry = searchResults[i];
+      let paragraph = document.createElement('p');
+      paragraph.textContent = entry;
+      resultsarea.appendChild(paragraph);
+    }
   }
 
   $.getJSON(url, data, callback);
